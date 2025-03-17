130a6a7ee4806c370f7e61cefbdbd06f66cb65cb
thisisvarun <varunthurram1@gmail.com>
2025-03-17 11:39:15 +0530

    refresh

--- Server/backend/Config/db_config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  email: "00varun2005@gmail.com",
-  password: "PASSWORD",
-  NEXMO_API_KEY: "API_KEY",
-  NEXMO_API_SECRET: "SECRET_KEY",
-  NEXMO_FROM_NUMBER: "NUMBER",
-  MongoURI: `mongodb+srv://land_admin:BDWbu0r8Jn6lDZ6j@cluster0.mongodb.net/land_registration?retryWrites=true&w=majority`
-}
--- migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-undef
-const LandRegistry = artifacts.require('LandRegistry')
-
-module.exports = function (deployer) {
-  deployer.deploy(LandRegistry)
-}
