e287706b1a59e9c75c3d8868b07e12cc2f53e51f
thisisvarun <varunthurram1@gmail.com>
2025-03-17 11:14:42 +0530

    0.0.1

--- Server/backend/Config/db_config.js
+++ Server/backend/Config/db_config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  email: "EMAIL",
+  email: "00varun2005@gmail.com",
   password: "PASSWORD",
   NEXMO_API_KEY: "API_KEY",
   NEXMO_API_SECRET: "SECRET_KEY",
   NEXMO_FROM_NUMBER: "NUMBER",
-  MongoURI: `ENTER_MONGOURI`
+  MongoURI: `mongodb+srv://land_admin:BDWbu0r8Jn6lDZ6j@cluster0.mongodb.net/land_registration?retryWrites=true&w=majority`
 }
