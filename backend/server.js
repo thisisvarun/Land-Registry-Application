599560108745321ec9ef7a56db4f5fa3a7647b34
thisisvarun <varunthurram1@gmail.com>
2025-03-17 19:58:16 +0530

    0.2.2

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Web3 setup (Ganache)
-const web3 = new Web3('http://127.0.0.1:7545');
+const web3 = new Web3('http://127.0.0.1:8545');
 
 // Signup
 app.post('/signup', async (req, res) => {
---
+++ backend/test.js
@@ -0,0 +1,3 @@
+const Web3 = require('web3');
+const web3 = new Web3('http://127.0.0.1:8545');
+console.log('Web3 version:', web3.version);
